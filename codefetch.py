4138a8609e562526a59e48cfa2f75ae9d0f807df
xPritam07 <pritambbk1982@gmail.com>
2025-04-08 23:14:44 +0530

    code update

--- codefetch.py
+++ codefetch.py
@@ -1,2 +1,2 @@
 from urllib.request import urlopen
-print(urlopen('https://raw.githubusercontent.com/xPritam07/Practical_codes/refs/heads/master/forward_backward.py').read().decode('utf-8'))
+print(urlopen('https://raw.githubusercontent.com/xPritam07/Practical_codes/refs/heads/master/ai/forward_backward.py').read().decode('utf-8'))
