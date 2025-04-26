799155c0f383b27c4fb1c6c744e6d13c40fe2106
xPritam07 <pritambbk1982@gmail.com>
2025-04-26 16:17:24 +0530

    code update

---
+++ bmlp/matrix.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+a=np.array([[2,3,4],
+            [4,2,6],
+            [6,7,8]])
+
+b=np.array([[5,2,7],
+            [8,3,9],
+            [1,4,9]])
+
+mat_add = np.add(a,b)
+print(mat_add)
