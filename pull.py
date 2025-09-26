c5e8ee1c1b134cdb14c876ecad0ce025194ae4b8
Pritam <pritambbk1982@gmail.com>
2025-09-26 13:05:44 +0530

    commit 4

--- pull.py
+++ pull.py
@@ -1,3 +1,5 @@
 import urllib.request
-print(urllib.request.urlopen("https://raw.githubusercontent.com/xPritam07/Practical_codes/refs/heads/master/DeepLearing/batch_normaliztion.py").read().decode('utf-8'))
+print(urllib.request.urlopen("https://raw.githubusercontent.com/xPritam07/Practical_codes/refs/heads/master/DeepLearing/xor.py").read().decode('utf-8'))
 
+# https://raw.githubusercontent.com/xPritam07/Practical_codes/refs/heads/master/DeepLearing/xor.py
+# https://raw.githubusercontent.com/xPritam07/Practical_codes/refs/heads/master/DeepLearning/xor.py
