1a8c4351ec2c7ef8d4912fda8ca7ac0e0018c5b5
Pritam <pritambbk1982@gmail.com>
2025-09-28 12:20:12 +0530

    cloud update

--- cloud_computing/feedback.py
+++ cloud_computing/feedback.py
@@ -1,22 +1,45 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
-feedback_list = []
+fd = []
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=['GET', 'POST'])
 def feedback():
-    if request.method == "POST":
-        fb = request.form["feedback"]
-        feedback_list.append(fb)
-    return '''
-    <h2>Feedback Form</h2>
-    <form method="post">
-        Enter Feedback: <input type="text" name="feedback" required><br>
-        <input type="submit" value="Submit">
-    </form>
-    <h3>Previous Feedback:</h3>
-    ''' + "<br>".join(feedback_list)
+    if request.method == 'POST':
+        feedb = request.form.get('feedback')
+        if len(feedb)>=5:
+            fd.append(feedb)
+            return redirect("/thank")
+        else:
+            return """
+                    <center>
+                    <h2>Give a vaild feedback</h2>
+                    <form method = 'post'>
+                        Feedback <input type="text" name="feedback" required><br>
+                        <input type='submit' value='Give Feedback'>
+                    </form>
+                    </center>
+                    """ 
+    return """
+            <center>
+            <h2>Feedback Form</h2>
+            <form method = 'post'>
+            Feedback <input type="text" name="feedback" required><br>
+            <input type='submit' value='Give Feedback'>
+            </form>
+            </center>
+            """
 
+@app.route("/thank")
+def thank_page():
+    return f"""
+            <center>
+            <h1>Thank you for your Feedback</h1>
+            <h2>You feedback is:</h2><br>
+            <p>{fd[-1]}</p>
+            <a href="/"><button>Go back</button></a>
+            </center>
+            """
 if __name__ == "__main__":
     app.run(debug=True)
--- test1.py
+++ test1.py
@@ -2,40 +2,21 @@ from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
-user_data = {
-    "admin":"admin123",
-    "Pritam": "pg5772",
-    "Susmita": "sg8701"
-}
-
-@app.route('/', methods = ['GET', 'POST'])
-def login():
+@app.route("/", methods=['GET', 'POST'])
+def feedback():
     if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        if username in user_data.keys() and password == user_data[username]:
-            return f"<center><h2>Welcome! You logged in successfully.<h2></center>"
-        else:
-            return'''
-<center>
-<h2>Wrong Credentials! Login to continue.<h2>
-<form method ='POST'>
-Username <input type="text" name="username" required /><br>
-Password <input type="password" name="password" required /><br>
-<input type="submit" value="Login">
-</from>
-</center>
-'''
+        q1 = request.form.get("q1")
+        score = 1 if q1 == "Cloud Computing" else 0
+        return f"<h2>The Score is {score}<h2>"
     return '''
-<center>
-<h2>Login Form<h2>
-<form method ='POST'>
-Username <input type="text" name="username" required /><br>
-Password <input type="password" name="password" required /><br>
-<input type="submit" value="Login">
-</from>
-</center>
+<h2>Simple Quiz</h2>
+    <form method="post">
+        Q1: Which technology provides on-demand computing resources over the internet?<br>
+        <input type="radio" name="q1" value="Cloud Computing"> Cloud Computing<br>
+        <input type="radio" name="q1" value="Networking"> Networking<br>
+        <input type="radio" name="q1" value="OS"> Operating System<br>
+        <input type="submit" value="Submit">
+    </form>
 '''
-
 if __name__ == "__main__":
     app.run(debug=True)
